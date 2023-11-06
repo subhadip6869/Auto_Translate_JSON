3382accd20c718794c1405d05765c8d833ef2b92
Subhadip Dutta <subhadip.dutta@quickelf.com>
2023-11-06 12:09:33 +0530

    removed comments for release

--- translate_json.py
+++ translate_json.py
@@ -6,12 +6,9 @@ import os
 translator = Translator()
 
 if __name__ == '__main__':
-    # from which index the command line arguments counting starts
-    # here the first argument for CLA is the file name
     CLA_START_INDEX = 1
 
     try:
-        # Load the source language json
         f_source = open(sys.argv[0+CLA_START_INDEX], "r", encoding="utf8")
         source_texts = json.load(f_source)
         f_source.close()
@@ -23,24 +20,18 @@ if __name__ == '__main__':
         sys.exit()
 
     try:
-        # Source language
         src_language = sys.argv[1+CLA_START_INDEX]
     except IndexError:
         print("Please specify source language")
         sys.exit()
 
-    # Languages in which the source language is to be translated
     translate_language_lists = sys.argv[2+CLA_START_INDEX:]
 
-    # Translating texts
     for lang in translate_language_lists:
         invalid_file = False
-        # Declaring a blank dictionary for storing the translations
         translated_texts = {}
-        curr, total = 0, len(source_texts) 
-        # Translating the values one by one using the google translator and storing inside the dictionary
+        curr, total = 0, len(source_texts)
         for key, value in source_texts.items():
-            # print("%s: %s" % (key, translator.translate(value, src=src_language, dest=lang).text))
             try:
                 curr += 1
                 print(f"Translating into {lang}: {curr}/{total}", end="\r")
@@ -50,22 +41,17 @@ if __name__ == '__main__':
                 invalid_file = True
                 break
             except Exception:
-                # if error occurs, try two more time to translate
                 try:
                     translated_texts[key] = translator.translate(value, src=src_language, dest=lang).text
                 except Exception:
                     try:
                         translated_texts[key] = translator.translate(value, src=src_language, dest=lang).text
                     except Exception:
-                        pass            
-
-        # print("Translated text: %s" % translated_texts)
+                        pass
 
         if not invalid_file:
             print("\nWriting into file...")
             os.makedirs(os.path.dirname(f"assets/{lang}.json"), exist_ok=True)
-            # Writing data into file from the dictionary
             f = open(f"assets/{lang}.json", mode="w", encoding="utf8")
             json.dump(translated_texts, f, indent=2, ensure_ascii=False)
             f.close()
-    
