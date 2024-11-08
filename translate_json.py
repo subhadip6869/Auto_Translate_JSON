b5eceb9bd01bf2086daa0a7c0944245b5adca65b
Subhadip Dutta <subhadip.dutta@quickelf.com>
2024-11-08 22:47:27 +0530

    modified code & added code for garbage translation cleaning

--- translate_json.py
+++ translate_json.py
@@ -1,10 +1,23 @@
+from json import JSONDecodeError
 from deep_translator import GoogleTranslator
 from deep_translator.exceptions import LanguageNotSupportedException
 import json
 import sys
 import os
 
-# translator = Translator()
+
+def map_language_key(lang_code):
+    if lang_code == 'he':
+        return 'iw'
+    elif lang_code == 'bh':
+        return 'bho'
+    elif lang_code == 'jv':
+        return 'jw'
+    elif lang_code == 'zh':
+        return 'zh-CN'
+    else:
+        return lang_code
+
 
 if __name__ == '__main__':
     try:
@@ -22,97 +35,133 @@ if __name__ == '__main__':
             print(f_error)
             sys.exit()
 
-        choice = input("1. Keep Existing Translations, 2. Clean & Translate, 3. Remove a translation: ")
+        # Automatically detect language files in the source directory
+        translate_language_lists = [os.path.splitext(file)[0] for file in os.listdir(source_path) if
+                                    file.endswith(".json")]
 
-        if choice == "1" or choice == "2" or choice == "3":
+        choice = input(
+            "1. Keep Existing Translations, 2. Clean & Translate, 3. Remove a translation, 4. Clean garbage translation: ")
 
-            if choice == "3":
-                remove_key = input("Enter text to be removed: ")
-
-            # Languages in which the source language is to be translated
-            # translate_language_lists = sys.argv[2:]
-
-            # Automatically detect language files in the source directory
-            translate_language_lists = [os.path.splitext(file)[0] for file in os.listdir(source_path) if file.endswith(".json")]
-            
-            # Translating texts
+        if choice == "1":
             for lang in translate_language_lists:
-                invalid_file = False
                 try:
-                    dst_lang = ''
-                    if lang == 'he':
-                        dst_lang = 'iw'
-                    elif lang == 'bh':
-                        dst_lang = 'bho'
-                    elif lang == 'jv':
-                        dst_lang = 'jw'
-                    elif lang == 'zh':
-                        dst_lang = 'zh-CN'
-                    else:
-                        dst_lang = lang
-                    translator = GoogleTranslator(source=src_language, target=dst_lang)
+                    if lang == src_language:
+                        continue
+                    translator = GoogleTranslator(source=src_language, target=map_language_key(lang))
+                    c_file = open(f"{source_path}/{lang}.json", "r", encoding="utf8")
+                    curr_texts = json.load(c_file)
+                    c_file.close()
+
+                    curr, total = 0, len(source_texts)
+
+                    # Declaring a blank dictionary for storing the translations
+                    translated_texts = {}
+                    for key, value in source_texts.items():
+                        curr += 1
+                        print(f"({lang}) Processed: {curr}/{total}", end="\r")
+
+                        try:
+                            translated_texts[key] = curr_texts[key]
+                        except KeyError:
+                            retry = 3
+                            while retry > 0:
+                                try:
+                                    translated_texts[key] = translator.translate(value)
+                                    break
+                                except LanguageNotSupportedException:
+                                    retry -= 1
+                            if retry == 0:
+                                print(f"Failed to translate: {key}")
+
+                    print("\nStoring translations...")
+                    with open(f"{source_path}/{lang}.json", mode="w", encoding="utf8") as f:
+                        json.dump(dict(sorted({**curr_texts, **translated_texts}.items())), f, indent=2,
+                                  ensure_ascii=False)
+
+                except FileNotFoundError as f_error:
+                    print(f_error)
+                except JSONDecodeError:
+                    print(f"Invalid JSON format in {lang}.json or the file is empty.")
                 except LanguageNotSupportedException:
-                    # if any extra json file is there which is not language file, then skip the loop
                     continue
 
-                # Declaring a blank dictionary for storing the translations
-                translated_texts = {}
+        elif choice == "2":
+            for lang in translate_language_lists:
+                try:
+                    if lang == src_language:
+                        continue
+                    translator = GoogleTranslator(source=src_language, target=map_language_key(lang))
+                    c_file = open(f"{source_path}/{lang}.json", "r", encoding="utf8")
+                    curr_texts = json.load(c_file)
+                    c_file.close()
 
-                if choice == "2":
-                    # when user wants to re-translate
-                    curr_texts = {}
-                else:
-                    # getting the current texts present in the file to skip repeated translations
-                    try:
-                        c_file = open(f"{source_path}/{lang}.json", "r", encoding="utf8")
-                        curr_texts = json.load(c_file)
-                        c_file.close()
-                    except FileNotFoundError:
-                        curr_texts = {}
+                    curr, total = 0, len(source_texts)
 
-                if choice == "3":
-                    curr_texts.pop(remove_key, "No key removed")
-                else:
-                    curr, total = 0, len(source_texts) 
-                    # Translating the values one by one using the google translator and storing inside the dictionary
+                    # Declaring a blank dictionary for storing the translations
+                    translated_texts = {}
                     for key, value in source_texts.items():
-                        # print("%s: %s" % (key, translator.translate(value, src=src_language, dest=lang).text))
-                        try:
-                            curr += 1
-                            print(f"({lang}) Processed: {curr}/{total}", end="\r")
-                            if not key in curr_texts.keys():
-                                # translated_texts[key] = translator.translate(value, src=src_language, dest=lang).text
-                                translated_texts[key] = translator.translate(value)
-                        except ValueError as v_error:
-                            print(f"\n{v_error}")
-                            invalid_file = True
-                            break
-                        except Exception:
-                            # if error occurs, try two more time to translate
+                        curr += 1
+                        print(f"({lang}) Processed: {curr}/{total}", end="\r")
+
+                        retry = 3
+                        while retry > 0:
                             try:
-                                if not key in curr_texts.keys():
-                                    # translated_texts[key] = translator.translate(value, src=src_language, dest=lang).text
-                                    translated_texts[key] = translator.translate(value)
-                            except Exception:
-                                try:
-                                    if not key in curr_texts.keys():
-                                        # translated_texts[key] = translator.translate(value, src=src_language, dest=lang).text
-                                        translated_texts[key] = translator.translate(value)
-                                except Exception as e:
-                                    print(f"{e} {key} {value}")        
+                                translated_texts[key] = translator.translate(value)
+                                break
+                            except LanguageNotSupportedException:
+                                retry -= 1
+                        if retry == 0:
+                            print(f"Failed to translate: {key}")
 
-                    # print("Translated text: %s" % translated_texts)
-
-                if not invalid_file:
                     print("\nStoring translations...")
-                    # Writing data into file from the dictionary
-                    f = open(f"{source_path}/{lang}.json", mode="w", encoding="utf8")
-                    json.dump(dict(sorted({**curr_texts, **translated_texts}.items())), f, indent=2, ensure_ascii=False)
-                    f.close()
-                # clearing the object
-                del translator
+                    with open(f"{source_path}/{lang}.json", mode="w", encoding="utf8") as f:
+                        json.dump(dict(sorted({**curr_texts, **translated_texts}.items())), f, indent=2,
+                                  ensure_ascii=False)
+
+                except FileNotFoundError as f_error:
+                    print(f_error)
+                except JSONDecodeError:
+                    print(f"Invalid JSON format in {lang}.json or the file is empty.")
+                except LanguageNotSupportedException:
+                    continue
+
+        elif choice == "3":
+            remove_key = input("Enter text to be removed: ")
+            for lang in translate_language_lists:
+                try:
+                    GoogleTranslator(source=src_language, target=map_language_key(lang))
+                    c_file = open(f"{source_path}/{lang}.json", "r", encoding="utf8")
+                    curr_texts = json.load(c_file)
+                    c_file.close()
+                    curr_texts.pop(remove_key, "No key removed")
+                    with open(f"{source_path}/{lang}.json", mode="w", encoding="utf8") as f:
+                        json.dump(curr_texts, f, indent=4)
+                except FileNotFoundError as f_error:
+                    print(f_error)
+                except JSONDecodeError:
+                    print(f"Invalid JSON format in {lang}.json or the file is empty.")
+                except LanguageNotSupportedException:
+                    continue
+
+        elif choice == "4":
+            for lang in translate_language_lists:
+                try:
+                    GoogleTranslator(source=src_language, target=map_language_key(lang))
+                    c_file = open(f"{source_path}/{lang}.json", "r", encoding="utf8")
+                    curr_texts = json.load(c_file)
+                    c_file.close()
+                    cleaned_texts = {key: value for key, value in curr_texts.items() if key in source_texts.keys()}
+                    with open(f"{source_path}/{lang}.json", mode="w", encoding="utf8") as f:
+                        json.dump(cleaned_texts, f, indent=4)
+                except FileNotFoundError as f_error:
+                    print(f_error)
+                except JSONDecodeError:
+                    print(f"Invalid JSON format in {lang}.json or the file is empty.")
+                except LanguageNotSupportedException:
+                    continue
+
         else:
             print("\nInvalid choice...")
-    
+
     except KeyboardInterrupt:
         print("\nProcess exited")
